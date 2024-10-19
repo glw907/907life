e2bcb5bd72c8ff4ffe9bd9528d25b169da89b0ae
Geoffrey L. Wright <geoff-login@907.life>
2024-10-19 21:42:34 +0000

    Set up 11ty and created basic site

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: "src",   // Source files folder
+      output: "docs"  // Output files to the /docs folder for GitHub Pages
+    }
+  };
+};
