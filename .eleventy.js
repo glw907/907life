456037d0d4b969078092757071cb90a180856160
Geoffrey L. Wright <geoff-login@907.life>
2024-10-20 01:05:54 +0000

    Set up Hugo site with PaperMod theme and initial content

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(eleventyConfig) {
-  return {
-    dir: {
-      input: "src",   // Source files folder
-      output: "docs"  // Output files to the /docs folder for GitHub Pages
-    }
-  };
-};
---
+++ content/posts/first-post.md
@@ -0,0 +1,7 @@
++++
+title = 'First Post'
+date = 2024-10-20T00:53:19Z
+draft = false
++++
+
+This is my first blog post on my Hugo site!
